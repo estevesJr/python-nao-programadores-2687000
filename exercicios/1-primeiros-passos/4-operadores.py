89c752bf687daf3e2c6ba97600c1fbe51d76ad54
estevesJr <maurilio.esteves@gmail.com>
2025-02-17 21:46:20 +0000

    Resolução do exercicio

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,9 +2,12 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+print (ano_formatura - ano_nascimento)
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
-
+print (ano_nascimento < ano_formatura)
+print (ano_nascimento <= ano_formatura)
+print (ano_nascimento == ano_formatura)
 
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+print(ano_formatura > ano_nascimento or ano_formatura > 2024)
