024710383ef5a617d77bb29d0fae0ec2f35efa00
estevesJr <maurilio.esteves@gmail.com>
2025-03-14 13:23:34 +0000

    Commit exercicio

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,19 @@
 # Crie uma lista apenas com elementos numéricos
-
+Lista_numeros = [1, 1, 2, 3, 5, 8, 13, 21]
+print (Lista_numeros)
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+Lista_mista = [1, "string", 3.1417, "14-08-1981", True, "string2"]
+print (Lista_mista)
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+print (Lista_mista[0:5])
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+print (Lista_mista[0:5:2])
 # Remova da lista o último item
+Lista_mista.pop()
 
 # Insira na lista um novo item
-
+Lista_mista.append ("novo item")
+print (Lista_mista)
 # Remova da lista um item específico
+Lista_mista.remove ("novo item")
+print (Lista_mista)
